0c20038204307095cfbccc2e46c1d7519b8e382b
Christoph Müller <mueller@olyro.de>
2022-01-17 18:05:17 +0100

    basic iso rendering

--- index.ts
+++ index.ts
@@ -17,15 +17,14 @@ type Position2d = {
 const width = 32;
 const height = 32;
 
-const W = 10;
-const H = 10;
+const SIZE = 10;
 
 const sprites: Array<Sprite> = [];
 
 
 const projectISOtoScreen: (p: Position3d) => Position2d  = p => {
   return {
-    x: p.x * width * 0.5 - p.y * width * 0.5 - width * 0.5 + W * 0.5 * width,
+    x: p.x * width * 0.5 - p.y * width * 0.5 - width * 0.5 + SIZE * 0.5 * width,
     y: 0.25 * p.x * height + 0.25 * p.y * height - p.z * height * 0.5
   }
 };
@@ -35,12 +34,19 @@ const image = new Image();
 if (canvas) {
   const ctx = canvas.getContext("2d");
 
+  canvas.addEventListener("mousemove", (event) => {
+    const y = (4 * event.offsetY - (height * event.offsetX * 2) / width + (SIZE + 1) * height) / (2 * height);
+    const x = (event.offsetX * 2) / width + y - SIZE - 1;
+
+    //console.log(Math.floor(x),Math.floor(y - 1));
+  })
+
   if (ctx) {
     image.onload = async function () {
       const grass = await createImageBitmap(image, 0, 0, width, height);
       const cube = await createImageBitmap(image, width, 0, width, height);
-      for (let i = 0; i < 10; i++) {
-        for (let j = 0; j < 10; j++) {
+      for (let i = 0; i < SIZE; i++) {
+        for (let j = 0; j < SIZE; j++) {
           sprites.push({
             pos: {
               x: i,
@@ -64,6 +70,12 @@ if (canvas) {
         },
         img: cube
       });
+      sprites.push({
+        pos: {
+          x: 5.5, y: 7.5, z: 1
+        },
+        img: cube
+      });
 
       sprites.push({
         pos: {
@@ -73,14 +85,15 @@ if (canvas) {
       });
 
       sprites.sort((a, b) => {
-        const aProjected = (a.pos.x + a.pos.y);
-        const bProjected = (b.pos.x + b.pos.y);
+        const aProjected = Math.ceil(a.pos.x) + Math.ceil(a.pos.y);
+        const bProjected = Math.ceil(b.pos.x) + Math.ceil(b.pos.y);
+
         if (aProjected < bProjected) {
           return -1;
         } else if (aProjected > bProjected) {
           return 1;
         } else {
-          if (a.pos.z < b.pos.z) { return -1 } else { return 1 };
+          if (a.pos.z < b.pos.z) { return -1 } else if (a.pos.z > b.pos.z) { return 1 } else {return 0};
         }
       });
 
